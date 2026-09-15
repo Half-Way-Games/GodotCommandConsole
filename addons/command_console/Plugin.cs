b4b983b908c660f5ce57be27a77734d36abbeb6d
Jake Pigg <Jakenbake97@gmail.com>
2026-09-15 12:43:22 -0500

    Setup plugin to register the dev console as an autoload and set an input action to activate it.

--- addons/command_console/Plugin.cs
+++ addons/command_console/Plugin.cs
@@ -1,17 +1,50 @@
 #if TOOLS
 using Godot;
+using Godot.Collections;
+namespace HWG.CommandConsole;
 
 [Tool]
 public partial class Plugin : EditorPlugin
 {
-	public override void _EnterTree()
-	{
-		// Initialization of the plugin goes here.
-	}
+    private static readonly StringName ConsoleAction = "DevConsole";
 
-	public override void _ExitTree()
-	{
-		// Clean-up of the plugin goes here.
-	}
+    public override void _EnterTree()
+    {
+        if (!InputMap.HasAction(ConsoleAction))
+        {
+            InputMap.AddAction(ConsoleAction);
+            var inputEvent = new InputEventKey
+            {
+                Keycode = Key.Quoteleft
+            };
+            InputMap.ActionAddEvent(ConsoleAction, inputEvent);
+
+            var actionSettings = new Dictionary
+            {
+                ["deadzone"] = 0.2f,
+                ["events"] = new Array<InputEvent> { inputEvent }
+            };
+
+            ProjectSettings.SetSetting("input/" + ConsoleAction, actionSettings);
+            ProjectSettings.Save();
+            Log.Info($"Command console added custom input action '{ConsoleAction}' bound to '`'");
+        }
+        
+        AddAutoloadSingleton("DevConsole", "res://addons/command_console/Scenes/DevConsole.tscn");
+    }
+
+    public override void _ExitTree()
+    {
+        if (InputMap.HasAction(ConsoleAction))
+        {
+            InputMap.EraseAction(ConsoleAction);
+            ProjectSettings.SetSetting("input/" + ConsoleAction, new Variant());
+            ProjectSettings.Save();
+            
+            Log.Info($"Command console removed custom input action '{ConsoleAction}' bound to '`'");
+        }
+        
+        RemoveAutoloadSingleton("DevConsole");
+    }
 }
 #endif
